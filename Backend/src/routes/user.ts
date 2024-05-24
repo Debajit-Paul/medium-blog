20871dce7d7e8e5a89389edc2e74903c99e19d0b
Debajit Paul <62774980+Debajit-Paul@users.noreply.github.com>
2024-05-25 02:55:11 +0530

    backend medium

---
+++ Backend/src/routes/user.ts
@@ -0,0 +1,88 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+import { signupInput } from "@deb/medium-common";
+
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+userRouter.post("/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const { success } = signupInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Input not correct",
+    });
+  }
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    const token = await sign(
+      {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      c.env.JWT_SECRET
+    );
+
+    return c.json({ token });
+  } catch (e) {
+    console.log(e);
+    c.status(403);
+    return c.text("Invalid");
+  }
+});
+
+userRouter.post("/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.text("User don't exist");
+    }
+
+    const token = await sign(
+      {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      c.env.JWT_SECRET
+    );
+
+    return c.json({ token });
+  } catch (error) {
+    c.status(403);
+    return c.text("Invalid");
+  }
+});
